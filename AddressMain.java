82d468f4237a64106a67e283dd431054151c609c
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-22 10:28:58 +0530

    Welcome Message

--- AddressMain.java
+++ AddressMain.java
@@ -5,10 +5,7 @@ public class AddressMain {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Address Book System");
-		ContactDetails contact = new ContactDetails("Chetan", "Udhane", "Pune", "Pune", "Maharashtra", 411039,
-				9172164415L, "chetanudhane2211@gmail.com");
-		System.out.println(contact);
-
+		
 	}
 
 }
