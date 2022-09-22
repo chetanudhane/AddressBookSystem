950e8ac306a02e0075d0681f595a92b532cd2fba
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-22 10:23:31 +0530

    Add Customer Details

--- AddressMain.java
+++ AddressMain.java
@@ -5,9 +5,10 @@ public class AddressMain {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Address Book System");
-		ContactDetails contact = new ContactDetails("Chetan", "Udhane", "Pune", "Pune", "Maharashtra", 411039,
-				9172164415L, "chetanudhane2211@gmail.com");
-		System.out.println(contact);
+
+		ABookSystem addressBook = new ABookSystem();
+		addressBook.addContacts();
+		System.out.println(addressBook.toString());
 
 	}
 
